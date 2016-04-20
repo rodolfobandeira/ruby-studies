7bf799b72233e1b04dd93dffa330655fbf57bcb1
rodolfobandeira <contato@rodolfobandeira.com>
2016-04-19 19:25:59 -0500

    more loops examples

---
+++ for-loop-2.rb
@@ -0,0 +1,7 @@
+# 1..15  prints 1 to 15
+# 1...15 prints 1 to 14
+
+for num in 1..15  
+  puts num
+end
+
---
+++ until-loop.rb
@@ -0,0 +1,7 @@
+counter = 1
+until counter == 10
+  puts counter
+  counter += 1
+end
+
+puts counter
