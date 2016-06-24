81ed228334cf9b9800ec7bde91b1fb0071bc7f58
rodolfobandeira <contato@rodolfobandeira.com>
2016-06-24 16:34:12 -0400

    ruby certification questions 4 and 8

---
+++ ruby-certification-question-4.rb
@@ -0,0 +1,7 @@
+t = Time.now + (60*60*24)
+p t
+
+# Outputs current system time + a day later
+#
+# 2016-06-25 16:32:25 -0400
+
--- ruby-certification-question-8.rb
+++ ruby-certification-question-8.rb
@@ -1,9 +1,6 @@
 # puts [1,1,2,3,5,8].compact｛|x| x*2}
-
 # puts [1,1,2,3,5,8].collect｛|x| x*2}
-
 # puts [1,1,2,3,5,8].map｛|x| x*2}
-
 # puts [1,1,2,3,5,8].flatten｛|x| x*2}
-
 # puts [1,1,2,3,5,8].join｛|x| x*2}
+#
