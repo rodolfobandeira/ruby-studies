78b64647c86593f2adaf1892ac512b5c94abab1a
rodolfobandeira <contato@rodolfobandeira.com>
2016-06-25 09:41:08 -0400

    arrays tests

---
+++ array-combining-two-arrays.rb
@@ -0,0 +1,17 @@
+leters = ['a', 'b', 'c', 'd']
+numbers = [1, 2, 3, 4]
+
+combined = leters + numbers
+
+puts combined
+
+# ruby array-combining-two-arrays.rb
+# a
+# b
+# c
+# d
+# 1
+# 2
+# 3
+# 4
+
---
+++ array-subtracting-one-array-from-another.rb
@@ -0,0 +1,11 @@
+leters = ['a', 'b', 'c', 'd']
+remove_piece = ['b', 'c']
+
+subtracted = leters - remove_piece
+
+puts subtracted
+
+# ruby array-subtracting-one-array-from-another.rb
+# a
+# d
+
